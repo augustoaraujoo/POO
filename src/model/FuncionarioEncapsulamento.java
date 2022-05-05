0df700e9c6e7bf84dc05bdd52713f1ac5e85c506
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-05-05 20:57:06 -0300

    refatorando a instância

--- src/model/FuncionarioEncapsulamento.java
+++ src/model/FuncionarioEncapsulamento.java
@@ -14,28 +14,13 @@ public class FuncionarioEncapsulamento {
         this.salario = salario;
     }
 
-    // sets
-    public void setCargo(String cargo) {this.cargo = cargo;}
-
-    public void setMatricula(int matricula) {this.matricula = matricula;}
-
-    public void setNome(String nome) {this.nome = nome;}
-
-    public void setSalario(double salario) {this.salario = salario;}
-    
-    
     //gets
     public int getMatricula() { return this.matricula; }
 
     public String getNome() { return this.nome; }
-
+    
     public String getCargo() { return this.cargo; }
 
     public double getSalario() { return this.salario; }
 
-    
-    
-    
-    
-    
 }
